a5aa6fdd34730bc8f44898ec4a31f4f94397820e
Tristan Lafontaine <tristanlaf003@gmail.com>
2024-10-24 12:59:10 -0400

    Ajout contrôle par websocket ou clavier

--- Sources/S5i-Projet-Godot/scripts/vehicle_body_3d.gd
+++ Sources/S5i-Projet-Godot/scripts/vehicle_body_3d.gd
@@ -2,7 +2,7 @@ extends VehicleBody3D
 
 @export var MAX_STEER = 0.9
 @export var ENGINE_POWER = 300
-
+const USE_WEBSOCKET = false 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,5 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta * 10)
-	engine_force = Input.get_axis("down","up") * ENGINE_POWER
+	if USE_WEBSOCKET :
+		pass
+	else :
+		steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta * 10)
+		engine_force = Input.get_axis("down","up") * ENGINE_POWER
