37a57302084416286c4305cd794778826007f7a7
Boie0601 <Boie0601@usherbrooke.ca>
2024-10-25 14:48:36 -0400

    Suiveur ligne marche sans code python

--- Sources/S5i-Projet-Godot/area_ligne_bleu.gd
+++ Sources/S5i-Projet-Godot/area_ligne_bleu.gd
@@ -21,12 +21,6 @@ func _on_capteur_ligne_1_area_entered(area: Area3D) -> void:
 	print("Capteur1") # Replace with function body.
 	properties[0] = 255
 
-
-func _on_capteur_ligne_5_area_entered(area: Area3D) -> void:
-	print("Capteur5") # Replace with function body.
-	properties[4] = 255
-	print(properties)
-
 func _on_capteur_ligne_2_area_entered(area: Area3D) -> void:
 	print("Capteur2") # Replace with function body.
 	properties[1] = 255
@@ -38,4 +32,31 @@ func _on_capteur_ligne_3_area_entered(area: Area3D) -> void:
 func _on_capteur_ligne_4_area_entered(area: Area3D) -> void:
 	print("Capteur4") # Replace with function body.
 	properties[3] = 255
+
+func _on_capteur_ligne_5_area_entered(area: Area3D) -> void:
+	print("Capteur5") # Replace with function body.
+	properties[4] = 255
+	print(properties)
+
+
+func _on_capteur_ligne_1_area_exited(area: Area3D) -> void:
+	properties[0] = 0 # Replace with function body.
+	print(properties)
+
+func _on_capteur_ligne_2_area_exited(area: Area3D) -> void:
+	properties[1] = 0 # Replace with function body.
+	print(properties)
+
+func _on_capteur_ligne_3_area_exited(area: Area3D) -> void:
+	properties[2] = 0 # Replace with function body.
+	print(properties)
+
+func _on_capteur_ligne_4_area_exited(area: Area3D) -> void:
+	properties[3] = 0 # Replace with function body.
+	print(properties)
+
+func _on_capteur_ligne_5_area_exited(area: Area3D) -> void:
+	properties[4] = 0 # Replace with function body.
+	print(properties)
+	
 	
