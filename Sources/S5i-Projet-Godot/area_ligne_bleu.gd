efac60a239ac87864f0635c9792ca1ca5c9fab02
Boie0601 <Boie0601@usherbrooke.ca>
2024-10-25 12:34:40 -0400

    Suiveur de ligne presque bon

---
+++ Sources/S5i-Projet-Godot/area_ligne_bleu.gd
@@ -0,0 +1,41 @@
+extends Area3D
+
+var properties = [0,0,0,0,0]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _get_property_list() -> Array[Dictionary]:
+	
+	return properties
+	
+
+func _on_capteur_ligne_1_area_entered(area: Area3D) -> void:
+	print("Capteur1") # Replace with function body.
+	properties[0] = 255
+
+
+func _on_capteur_ligne_5_area_entered(area: Area3D) -> void:
+	print("Capteur5") # Replace with function body.
+	properties[4] = 255
+	print(properties)
+
+func _on_capteur_ligne_2_area_entered(area: Area3D) -> void:
+	print("Capteur2") # Replace with function body.
+	properties[1] = 255
+
+func _on_capteur_ligne_3_area_entered(area: Area3D) -> void:
+	print("Capteur3") # Replace with function body.
+	properties[2] = 255
+
+func _on_capteur_ligne_4_area_entered(area: Area3D) -> void:
+	print("Capteur4") # Replace with function body.
+	properties[3] = 255
+	
--- Sources/S5i-Projet-Godot/scripts/vehicle_body_3d.gd
+++ Sources/S5i-Projet-Godot/scripts/vehicle_body_3d.gd
@@ -6,11 +6,6 @@ const Lignes = preload("res://LigneNoir.gd")
 @export var MAX_STEER = 0.9
 @export var ENGINE_POWER = 300
 const USE_WEBSOCKET = false 
-var cap1
-var cap2
-var cap3
-var cap4
-var cap5
 var xy_cap1
 var xy_cap2
 var xy_cap3
@@ -31,22 +26,6 @@ func _process(delta: float) -> void:
 	else :
 		steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta * 10)
 		engine_force = Input.get_axis("down","up") * ENGINE_POWER
-		
-func get_pos_capteur() -> Array:
-	cap1 = get_node("CapteurLigne1")
-	cap2 = get_node("CapteurLigne2")
-	cap3 = get_node("CapteurLigne3")
-	cap4 = get_node("CapteurLigne4")
-	cap5 = get_node("CapteurLigne5")
-	
-	xy_cap1 = cap1.get_world_3d()
-	xy_cap2 = cap2.get_world_3d()
-	xy_cap3 = cap3.get_world_3d()
-	xy_cap4 = cap4.get_world_3d()
-	xy_cap5 = cap5.get_world_3d()
-	return [xy_cap1, xy_cap2, xy_cap3, xy_cap4, xy_cap5]
-	
-	print(xy_cap1)
 	
 	
 	
