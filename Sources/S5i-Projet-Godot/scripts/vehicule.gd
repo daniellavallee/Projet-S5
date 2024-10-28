29df8a970be1f396aa5183776043eae67bced33a
Tristan Lafontaine <tristanlaf003@gmail.com>
2024-10-28 11:08:09 -0400

    Amélioration du déplacement en angle

--- Sources/S5i-Projet-Godot/scripts/vehicule.gd
+++ Sources/S5i-Projet-Godot/scripts/vehicule.gd
@@ -2,8 +2,8 @@ extends VehicleBody3D
 class_name Vehicule
 
 @export var MAX_STEER = 1
-@export var ENGINE_POWER = 150
-const USE_WEBSOCKET = true 
+@export var ENGINE_POWER = 200
+const USE_WEBSOCKET = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,9 +15,10 @@ func _process(delta: float) -> void:
 	if USE_WEBSOCKET :
 		# Direction des roues
 		var radian = (90 - GlobalData.wheel_angle) * PI / 180
-		steering = move_toward(steering, radian  * MAX_STEER, delta * 10)
+		steering = move_toward(steering, radian  * MAX_STEER, delta * 20)
 		# Avance ou recule
 		engine_force = GlobalData.bw_speed / 100 * ENGINE_POWER
+		print(engine_force)
 	else :
 		steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta * 10)
 		engine_force = Input.get_axis("down","up") * ENGINE_POWER
