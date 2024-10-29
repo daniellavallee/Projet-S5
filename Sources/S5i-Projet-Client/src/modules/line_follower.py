ef0fd9291cbd7b49a25a5d27950daef46269db36
Vikil1001 <vincent.kilanowski@gmail.com>
2024-10-29 12:45:31 -0400

    gestion perte de la ligne

--- Sources/S5i-Projet-Client/src/modules/line_follower.py
+++ Sources/S5i-Projet-Client/src/modules/line_follower.py
@@ -2,56 +2,59 @@ from src.models import LineFollowerConfig, RaspberryPiResponse, ControllerRespon
 from src.enums import RunStates
 from .motors import Motors
 
+
 class LineFollower():
+    lastValue = [0, 0, 0, 0, 0]
     """
     Class responsible for handling the line follower sensor
     """
-    def __init__(self, config:LineFollowerConfig, motors_module: Motors,verbose:bool=False):
+
+    def __init__(self, config: LineFollowerConfig, motors_module: Motors, verbose: bool = False):
         self.config = config
         self.verbose = verbose
         self.off_track_count = 0
         self.turning_angle = 90
         self.motors_module = motors_module
 
-    def read(self, rpi_response:RaspberryPiResponse) -> list[bool]:
+    def read(self, rpi_response: RaspberryPiResponse) -> list[bool]:
         digital_values = [value >= self.config.min_white for value in rpi_response.line_follower]
         if self.verbose:
-            print("Line follower | Analog values : ",digital_values)
-            print("Line follower | Digital values : ",digital_values)
+            print("Line follower | Analog values : ", digital_values)
+            print("Line follower | Digital values : ", digital_values)
             print("==============================")
         return digital_values
-        
-    def run_follower(self, rpi_response:RaspberryPiResponse) -> RunStates:
+
+    def run_follower(self, rpi_response: RaspberryPiResponse) -> RunStates:
         values = self.read(rpi_response)
-        cr = ControllerResponse(0,0)
+        cr = ControllerResponse(0, 0)
         a_step = 3
         b_step = 10
         c_step = 30
         d_step = 45
         # Angle calculate
-        if values == [0,0,1,0,0]:
+        if values == [0, 0, 1, 0, 0]:
             step = 0
-        elif values == [0,1,1,0,0] or values == [0,0,1,1,0]:
+        elif values == [0, 1, 1, 0, 0] or values == [0, 0, 1, 1, 0]:
             step = a_step
-        elif values == [0,1,0,0,0] or values == [0,0,0,1,0]:
+        elif values == [0, 1, 0, 0, 0] or values == [0, 0, 0, 1, 0]:
             step = b_step
-        elif values == [1,1,0,0,0] or values == [0,0,0,1,1]:
+        elif values == [1, 1, 0, 0, 0] or values == [0, 0, 0, 1, 1]:
             step = c_step
-        elif values == [1,0,0,0,0] or values == [0,0,0,0,1]:
+        elif values == [1, 0, 0, 0, 0] or values == [0, 0, 0, 0, 1]:
             step = d_step
         # Direction calculate
-        if values == [0,0,1,0,0]:
+        if values == [0, 0, 1, 0, 0]:
             self.off_track_count = 0
             self.turning_angle = 90
-		# turn right
-        elif values in ([0,1,1,0,0],[0,1,0,0,0],[1,1,0,0,0],[1,0,0,0,0]):
+        # turn right
+        elif values in ([0, 1, 1, 0, 0], [0, 1, 0, 0, 0], [1, 1, 0, 0, 0], [1, 0, 0, 0, 0]):
             self.off_track_count = 0
             self.turning_angle = int(90 - step)
-		# turn left
-        elif values in ([0,0,1,1,0],[0,0,0,1,0],[0,0,0,1,1],[0,0,0,0,1]):
+        # turn left
+        elif values in ([0, 0, 1, 1, 0], [0, 0, 0, 1, 0], [0, 0, 0, 1, 1], [0, 0, 0, 0, 1]):
             self.off_track_count = 0
             self.turning_angle = int(90 + step)
-        elif values == [0,0,0,0,0]:
+        elif values == [0, 0, 0, 0, 0]:
             self.off_track_count += 1
             if self.off_track_count > 10:
                 return RunStates.FINDING_LINE
@@ -60,27 +63,25 @@ class LineFollower():
 
         self.motors_module.set_angle(self.turning_angle)
         self.motors_module.set_speed(self.config.cruising_speed)
-        
+        self.lastValue = values
         return RunStates.LINE_FOLLOWING
-    
-    def found_line(self, values:list[bool]) -> RunStates:
-        if values == [0,0,0,0,0]:
+
+    def found_line(self, values: list[bool]) -> RunStates:
+        if values == [0, 0, 0, 0, 0]:
             return RunStates.FINDING_LINE
         return RunStates.LINE_FOLLOWING
-    def run_finder(self, rpi_response:RaspberryPiResponse) -> RunStates:
+
+    def run_finder(self, rpi_response: RaspberryPiResponse) -> RunStates:
+
         values = self.read(rpi_response)
-        
-        self.off_track_count += 1
-        center_angle = self.motors_module.config.maxRightAngle - self.motors_module.config.maxLeftAngle
-        if self.off_track_count > self.config.max_off_track_count:
-            tmp_angle = (self.turning_angle-center_angle)/abs(center_angle-self.turning_angle)
-            tmp_angle *= self.motors_module.config.maxLeftAngle
-            tmp_angle += center_angle
-            
-            self.motors_module.set_angle(int(tmp_angle))
-            self.motors_module.set_speed(self.config.finders_speed)
-        else:
-            self.motors_module.set_angle(center_angle)
-            self.motors_module.set_speed(0)
+        step = 45
+
+        if self.lastValue == [0, 0, 0, 1, 0] or self.lastValue == [0, 0, 0, 0, 1]:
+            self.turning_angle = int(90 + step)
+
+        if self.lastValue == [0, 1, 0, 0, 0] or self.lastValue == [1, 0, 0, 0, 0] or self.lastValue == [0, 0, 1, 0, 0]:
+            self.turning_angle = int(90 - step)
+
+        self.motors_module.set_angle(self.turning_angle)
+        self.motors_module.set_speed(self.config.finders_speed)
         return self.found_line(values)
-            
