576f3d9e0e596c8e8ea17edd2726bdb317d78935
Boie0601 <Boie0601@usherbrooke.ca>
2024-10-29 11:19:35 -0400

    Mod capteur raycast fonctionne

---
+++ Sources/S5i-Projet-Godot/capteur_ligne_3.gd
@@ -0,0 +1,16 @@
+extends RayCast3D
+
+@onready var ligne: StaticBody3D = $"../../AreaLigneBlanche/Ligne"
+@onready var plancher: StaticBody3D = $"../../AreaPlancher/Plancher"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_colliding():
+		if get_collider() == ligne:
+			GlobalData.line[2] = 0
+		else: if get_collider() == plancher:
+			GlobalData.line[2] = 255
