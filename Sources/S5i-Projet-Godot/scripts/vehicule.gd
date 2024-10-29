778e10187f1838fa1e732d915636daeff28d9c8e
Tristan Lafontaine <tristanlaf003@gmail.com>
2024-10-29 12:21:13 -0400

    Ajustement du capteur pour obtenir zéro

--- Sources/S5i-Projet-Godot/scripts/vehicule.gd
+++ Sources/S5i-Projet-Godot/scripts/vehicule.gd
@@ -3,7 +3,7 @@ class_name Vehicule
 
 @export var MAX_STEER = 0.6
 @export var ENGINE_POWER = 150
-const USE_WEBSOCKET = false
+const USE_WEBSOCKET = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
