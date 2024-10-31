b5151a244daaedc906d6cc46fd897280af43855a
Tristan Lafontaine <tristanlaf003@gmail.com>
2024-10-31 15:24:24 -0400

    [Feat] Movement is natural

--- Sources/S5i-Projet-Godot/scripts/vehicule.gd
+++ Sources/S5i-Projet-Godot/scripts/vehicule.gd
@@ -1,26 +1,42 @@
-extends VehicleBody3D
-class_name Vehicule
+extends CharacterBody3D
 
-@export var MAX_STEER = 0.6
-@export var ENGINE_POWER = 30
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var speed_multiplier = 0.1
+@export var angle_multiplier = 0.1
+
+
+
 @export var USE_WEBSOCKET = true
-@onready var bille: RigidBody3D = $"../Bille"
-var BREAK: bool = false
-# Called when the node enters the scene tree for the first time.
+
+@onready var roueAvantDroit = $"AvantDroit"
+@onready var roueAvantGauche = $"AvantGauche"
+
 func _ready() -> void:
 	pass # Replace with function body.
 
+func physic(controle_angle:float,controle_moteur:float):
+	var angle_roues = deg_to_rad(45) * controle_angle * angle_multiplier
+	var up = Vector3(0,1,0)
+	if controle_moteur < 0:
+		up *= -1
+	var forward = Vector3(0,0,1)
+	var new_forward = Vector3(sin(angle_roues),0,cos(angle_roues)) * forward
+	print(new_forward)
+	var vel = new_forward * controle_moteur * speed_multiplier
+	var magn = vel.length()
+	translate(vel)
+	rotate(up,angle_roues*magn)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 	if Input.is_action_pressed("reset"):
 		get_parent().get_tree().reload_current_scene()
 	if USE_WEBSOCKET :
-		# Direction des roues
-		var radian = (90 - GlobalData.wheel_angle) * PI / 180
-		steering = move_toward(steering, radian  * MAX_STEER, delta * 20)
-		# Avance ou recule
-		engine_force = GlobalData.bw_speed / 100 * ENGINE_POWER
+		pass
 	else :
-		steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta * 2.5)
-		engine_force = Input.get_axis("down","up") * ENGINE_POWER
+		var controle_angle = Input.get_axis("right","left")
+		var controle_moteur = Input.get_axis("down", "up")
+		physic(controle_angle,controle_moteur)
+		
+		
+	
