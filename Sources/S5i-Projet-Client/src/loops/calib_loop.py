9494795fe8df9f75d13cd0ae1d7c3d3dbaa10ae5
Mathieu Sevegny <msevegny@machinex.ca>
2024-10-31 11:08:32 -0400

    [Feat] Made it work great

--- Sources/S5i-Projet-Client/src/loops/calib_loop.py
+++ Sources/S5i-Projet-Client/src/loops/calib_loop.py
@@ -5,33 +5,38 @@ from src.models import ControllerResponse, RaspberryPiResponse
 from src.enums import Hosts
 from src.constants import SAMPLES_PATH
 import keyboard
+from datetime import timedelta
 
 class CalibLoop(BaseLoop):
     def __init__(self, host: Hosts = False) -> None:
         super().__init__(host, is_verbose=False)
-        self.samples : list[float] = []
+        self.samples : list[tuple[timedelta,int]] = []
         self.max_num_samples = 30
         if not SAMPLES_PATH.exists():
             with SAMPLES_PATH.open('w') as f:
-                title = "Sample Name, "
+                title = "Sample Name,Delta time,"
                 for i in range(self.max_num_samples):
                     title += f"Sample {i},"
                 title = title[:-1]
                 title += "\n"
                 f.write(title)
-    def control(self, rpi_response: RaspberryPiResponse) -> ControllerResponse:
+    def control(self, rpi_response: RaspberryPiResponse):
         sys.stdout.flush()
         print(rpi_response, end='\r')
-        self.samples.append(rpi_response.sonar)
-        if len(self.samples) >= self.max_num_samples:
+        self.samples.append([self.time_module.dt,rpi_response.sonar])
+        if len(self.samples) > self.max_num_samples:
             self.samples.pop(0)
         if keyboard.is_pressed('space'):
+            if len(self.samples) < self.max_num_samples:
+                print("Not enough samples")
+                return
             sys.stdout.flush()
             sample_name = input("\nEnter sample name:")
             
             with SAMPLES_PATH.open('a') as f:
-                f.write(f"{sample_name.strip()}")
+                f.write(f"{sample_name.strip()},")
+                f.write(f"{sum([sample[0].total_seconds() for sample in self.samples])/len(self.samples)}")
                 for sample in self.samples:
-                    f.write(f",{sample}")
+                    f.write(f",{sample[1]}")
                 f.write("\n")
-        
+            self.samples = []
