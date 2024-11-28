66147cb4eb5088d4ae1c6f8756268c1970de4544
Mathieu Sevegny <sevegny.mathieu@gmail.com>
2024-11-28 10:59:44 -0500

    New Modifs

--- Sources/S5i-Projet-Client/src/modules/line_follower.py
+++ Sources/S5i-Projet-Client/src/modules/line_follower.py
@@ -61,7 +61,7 @@ class LineFollower():
         # Angle calculate
         if values == [0, 0, 1, 0, 0]:
             step = 0
-        elif values == [0, 1, 1, 0, 0] or values == [0, 0, 1, 1, 0]:
+        elif values == [0, 1, 1, 0, 0] or values == [0, 0, 1, 1, 0] or values == [0,0,1,0,0]:
             step = a_step
         elif values == [0, 1, 0, 0, 0] or values == [0, 0, 0, 1, 0]:
             step = b_step
