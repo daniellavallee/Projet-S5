4d4dd7452a3914dfa47ef657534f695a79c401fe
Mathieu Sevegny <sevegny.mathieu@gmail.com>
2024-12-02 12:24:59 -0500

    New Modifs

--- Sources/S5i-Projet-PiCar/src/controller.py
+++ Sources/S5i-Projet-PiCar/src/controller.py
@@ -21,9 +21,9 @@ def control_back_wheels(speed:int, angle:int):
         right_speed = speed
     elif (angle<90): # Gauche
         left_speed = speed
-        right_speed = speed_of_bracked_wheel
+        right_speed = 1
     else:
-        left_speed = speed_of_bracked_wheel
+        left_speed = 1
         right_speed = speed
         
     if (left_speed>0):
